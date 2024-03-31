14519fde221983e151e811ba38356b10dfe8394e
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2024-03-31 17:45:58 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+set -e
+
 clang-format -i src/* include/*
+
+mkdir -p build
 cd build
+
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug ..
 mv compile_commands.json ../
+
 make
